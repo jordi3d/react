d47a5052ad6fa429b470662751a27d5881bcd893
jordi3d <jordi@cine3d.com>
2022-02-03 20:13:36 +0100

    primer component afegit

---
+++ hello-world/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import HelloWorld from "./HelloWorld.jsx";
+
+export default function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+      <p>
+        This is my first <br />
+        page
+      </p>
+    </div>
+  );
+}
