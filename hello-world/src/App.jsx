e7161d32758cba50188bafcbd7802ac2fafbb51e
jordi3d <jordi@cine3d.com>
2022-02-03 20:45:32 +0100

    afegint children

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Hello from "./Hello";
 
 import HelloWorld from "./HelloWorld";
 
 export default function App() {
+  let john = "juanito";
   return (
     <div className="App">
       <HelloWorld />
+      <Hello nom="Pere" />
+      <Hello nom={john} />
+      <Hello nom={<em>Paul</em>} />
       <p>
         This is my second <br />
         page
---
+++ hello-world/src/Box.jsx
@@ -0,0 +1,5 @@
+import "./Box.css";
+
+export default function Box({ children }) {
+  return <div className="box">{children}</div>;
+}
